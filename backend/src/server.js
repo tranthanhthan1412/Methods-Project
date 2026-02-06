0d28195c830e0cb3e69e20a074675c388d537d90
tranthanhthan1412 <tranthanhthang333@gmail.com>
2026-02-06 15:17:45 +0700

    chuan bi deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,28 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import taskRoute from "./routes/tasksRoutes.js";
 import cors from "cors";
+import path from "path";
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // cai dat thu muc chua file build frontend
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); // phuc vu file index.html
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
